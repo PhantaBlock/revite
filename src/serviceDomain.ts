3545a020622b42a19eb88c70ce64631cd6c3aa38
W2F <565009731@qq.com>
2023-07-06 15:00:48 +0800

    temp

--- src/serviceDomain.ts
+++ src/serviceDomain.ts
@@ -1,3 +1,3 @@
 export const REVITE_DOMAIN = window.__SERVICE_DOMAIN__
-    ? `http://${window.__SERVICE_DOMAIN__?.["REVOLT"]}`
+    ? `https://${window.__SERVICE_DOMAIN__?.["REVOLT"]}`
     : import.meta.env.VITE_API_URL;
