d3ddd42c7b641bc351d6036028eadd5a5444a49d
W2F <565009731@qq.com>
2023-07-06 15:37:38 +0800

    feat: domainMapping

--- src/serviceDomain.ts
+++ src/serviceDomain.ts
@@ -1,3 +1,5 @@
+const protocol = window.__SKY_CLIENT_ENV__ === "daily" ? "http" : "https";
+
 export const REVITE_DOMAIN = window.__SERVICE_DOMAIN__
-    ? `https://${window.__SERVICE_DOMAIN__?.["REVOLT"]}`
+    ? `${protocol}://${window.__SERVICE_DOMAIN__?.["REVOLT"]}`
     : import.meta.env.VITE_API_URL;
