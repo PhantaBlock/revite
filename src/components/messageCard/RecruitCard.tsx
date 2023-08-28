e8e1ebb55af977e829d985e607c02bed34b69d25
W2F <565009731@qq.com>
2023-08-28 20:46:04 +0800

    feat: 招募信息处理

--- src/components/messageCard/RecruitCard.tsx
+++ src/components/messageCard/RecruitCard.tsx
@@ -7,17 +7,28 @@ interface IMessage {
     currentPlayer: number;
     maxPlayer: number;
     gameType: string;
-    gameMode: string;
+    matchMode: string;
     mapName: string;
     mapImg: string;
 }
 
+const QuickMatchImg = 'https://skyvs.oss-cn-hangzhou.aliyuncs.com/resources/images/icons.jpeg';
+const matchModeMap: any = {
+    OneVsOne: '1v1',
+    TwoVsTwo: '2v2',
+    FourVsFour: '4v4',
+};
+
 const RecruitCard = ({ data }: { data: IMessage }) => {
+    const isQuickMatch = data.roomType === 'QuickMatch';
+
     const handleClick = () => {
         window.__JOIN_ROOM__?.({
             game_type: data.gameType,
             room_id: Number(data.roomId),
         }).then((res: any) => {
+            window.__TOAST__?.('加入成功');
+
             if (window.__SKY__) {
                 window.__SKY__.activePage();
                 window.__SKY__.postEvent({
@@ -33,16 +44,16 @@ const RecruitCard = ({ data }: { data: IMessage }) => {
     return (
         <div className={styles.RecruitCard} onClick={handleClick}>
             <div className={styles.imgWrap}>
-                <img src={data.mapImg} alt=" " />
+                <img src={isQuickMatch ? QuickMatchImg : data.mapImg} alt=" " />
             </div>
             <div className={styles.infoWrap}>
                 <div className={styles.mapName}>
-                    <div className={styles.ellipsis}>{data.mapName}</div>
+                    <div className={styles.ellipsis}>{isQuickMatch ? 'RTS娱乐匹配' : data.mapName}</div>
                     <div className={styles.fixed}>(<span>{data.currentPlayer}</span>/{data.maxPlayer})</div>
                 </div>
                 <div className={styles.otherInfo}>
                     <div>{data.gameType === 'War3' ? 'RTS' : data.gameType}</div>
-                    <div>{data.gameMode}</div>
+                    <div>{matchModeMap[data.matchMode]}</div>
                 </div>
             </div>
             <div className={styles.joinBtn}>加入</div>
